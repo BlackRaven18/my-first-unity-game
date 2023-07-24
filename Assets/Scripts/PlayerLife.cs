9bb7e4d70a83b051e35e21e4251b3ce005ea5cca
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2023-07-24 10:07:58 +0200

    feat: player death, traps

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+
+    private Animator playerAnimator;
+    private Rigidbody2D playerRigidbody;
+
+    private void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        playerRigidbody.bodyType = RigidbodyType2D.Static;
+        playerAnimator.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
