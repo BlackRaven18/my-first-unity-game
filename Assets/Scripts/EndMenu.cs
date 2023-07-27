0b406c9fe3a565fafdfc3741830563505c757a94
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2023-07-27 13:35:08 +0200

    refactor: end screen

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndMenu : MonoBehaviour
 {
+    [SerializeField] private Text cherriesTextField;
+
+    private void Start()
+    {
+        cherriesTextField.text += MainManager.Instance.cherries;
+    }
     public void Quit()
     {
         Application.Quit();   
