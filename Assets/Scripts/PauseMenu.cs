3bce0e136939f498b9a9abb27e3bfd263fbc1302
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-12-30 21:04:42 +0100

    fix: sound volume slider

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,11 +6,12 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
 
-    private MainManager mainManager = MainManager.Instance;
+    private MainManager mainManager;
     [SerializeField] private GameObject pauseMenu;
     // Start is called before the first frame update
     void Start()
     {
+        mainManager = MainManager.Instance;
         pauseMenu.SetActive(false);
     }
 
